aeb09ed0d2c47b2832e5f6c26990cc730eb86ff4
Scottie Biddle <smb510@gmail.com>
2012-09-20 23:54:07 -0400

    worrrrd

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -11,12 +11,13 @@ class AdminController < ApplicationController
   def tally
     @ballots = Ballot.all
     @candidates = Candidate.all
-    #@candidates.each do |c|
-     # c.yes = 0
-    #  c.no = 0
-    #  c.abstain = 0
-    #  c.save
-    #end
+    @candidates.each do |c|
+      c.yes = 0
+     c.no = 0
+     c.abstain = 0
+     c.save
+    end
+    @candidates = Candidate.all
     @ballots.each do |ballot|
       ballot.votes.each {|candidate_id, vote|
         candidate = @candidates.select{|s| s.id == candidate_id.to_i}.first
