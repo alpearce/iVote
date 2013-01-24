97135e4f732041f399a0ab6606858b08b490aff2
Scottie Biddle <smb510@gmail.com>
2013-01-24 00:21:55 -0500

    debug current users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
       user = User.from_omniauth(env["omniauth.auth"])
       session[:user_id] = user.id
-      redirect_to "/voting/quickpick"
+      redirect_to root_url
     end
     
   def destroy
