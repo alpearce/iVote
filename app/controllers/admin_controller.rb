13b4ea4aae156ef199c4dd408cf0b0aea9a8fc99
Scottie Biddle <smb510@gmail.com>
2013-01-30 15:50:14 -0500

    full results

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -121,7 +121,7 @@ class AdminController < ApplicationController
     @vote_count = UserVoted.all.count
     @candidates = Candidate.all
     @candidates.each do |candidate|
-      if candidate.yes >= 39 || candidate.no >= 7 * @vote_count
+      if candidate.yes >= 50 || candidate.no >= 9 * @vote_count
         candidate.delete
       else
         candidate.yes = 0
