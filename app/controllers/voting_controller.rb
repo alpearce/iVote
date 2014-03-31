a1aef45fa02e8cbba1c7ead57758bdf7758cfd7d
Lily  Wang <lilw@Lilys-MacBook-Pro-3.local>
2014-03-31 00:11:42 -0400

    updated controller

--- app/controllers/voting_controller.rb
+++ app/controllers/voting_controller.rb
@@ -29,8 +29,8 @@ class VotingController < ApplicationController
       #YOU ALSO HAVE TO CHANGE THESE NUMBERS WHEN CHANGING YES VOTES
       if yes_validation.size > 18
         respond_to do |format|
-          format.html {render :text => "You may not have more than 18 YES votes. You had" + yes_validation.size.to_s, :status => 406}
-          format.json {render :text => "You may not have more than 18 YES votes.", :status => 403}
+          format.html {render :text => "You may not have more than 11 YES votes. You had" + yes_validation.size.to_s, :status => 406}
+          format.json {render :text => "You may not have more than 11 YES votes.", :status => 403}
         end
         return
       end
